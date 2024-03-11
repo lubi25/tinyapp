311acb25724e7e108536c2d806e14012e96d1a5f
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-11 00:58:55 -0400

    Implement generateRandomString

--- express_server.js
+++ express_server.js
@@ -43,5 +43,10 @@ app.post("/urls", (req, res) => {
 });
 
 function generateRandomString() {
-  
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
