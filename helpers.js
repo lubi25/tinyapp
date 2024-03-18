f101fd68b676422e24c9662104b49b106875d820
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-17 22:47:02 -0400

    Move 'generate' functions from index

--- helpers.js
+++ helpers.js
@@ -6,6 +6,47 @@ function getUserByEmail(email, database) {
     }
   }
   return null;
+};
+
+
+function generateRandomKey(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomKey = '';
+  for (let i = 0; i < length; i++) {
+    randomKey += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomKey;
+};
+
+
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let shortURL = '';
+  for (let i = 0; i < 6; i++) {
+    shortURL += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return shortURL;
+};
+
+
+function generateRandomID() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomUserID = '';
+  for (let i = 0; i < 12; i++) {
+    randomUserID += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomUserID;
+};
+
+
+function urlsForUser(id) {
+  const userURLs = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userURLs;
 }
 
-module.exports = getUserByEmail;
+module.exports = { getUserByEmail, generateRandomKey, generateRandomString, generateRandomID, urlsForUser };
